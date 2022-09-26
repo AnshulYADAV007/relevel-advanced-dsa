322476652bad1be5aa224af67fff870f3b84b0ab
AnshulYADAV <anshulyadav98@gmail.com>
2022-09-26 22:46:39 +0530

    did painter and book as well

--- 8. Advanced Problem Solving in Binary Search(26th Sept 2022)/problems.js
+++ 8. Advanced Problem Solving in Binary Search(26th Sept 2022)/problems.js
@@ -7,10 +7,10 @@
  * @retruns the maximum number of cows you can accomodate
  */
 let maximumCowCount = function(stalls, minimumDistance) {
-    let answer = 1, prev = stalls[0]
+    let answer = 1, curr = stalls[0]
     for (let next of stalls.slice(1)) {
-        if (next - prev >= minimumDistance) {
-            prev = next
+        if (next - curr >= minimumDistance) {
+            curr = next
             answer++
         }
     }
@@ -23,6 +23,8 @@ let largestMinimumDistance = function(stalls, cowCount) {
     let lo = 1, hi = stalls[stalls.length - 1] - stalls[0], answer = -1
     while(lo <= hi) {
         let mid = Math.floor(lo + (hi - lo) / 2)
+        console.log(`lo : ${lo}, mid : ${mid}, hi : ${hi},` +
+                    `maximumCowCount : ${maximumCowCount(stalls, mid)}`)
         if (maximumCowCount(stalls, mid) >= cowCount) {
             answer = mid
             lo = mid + 1
@@ -33,4 +35,45 @@ let largestMinimumDistance = function(stalls, cowCount) {
     return answer
 }
 
-console.log(largestMinimumDistance([1,2,4,8,9], 3))
+// console.log(largestMinimumDistance([1,2,4,8,9], 3))
+
+/** 
+ * Painter Partition 1 
+ * Book Allocation
+ * 
+ * In both these questions you are given an array. You need to partition the array 
+ * into p partitions. Such that the maixum partition sum is minimized.
+ */
+
+let minPartitionCount = function(nums, maxSum) {
+    let answer = 1, currentSum = 0
+    for(let num of nums) {
+        if (currentSum + num <= maxSum) {
+            currentSum += num
+        } else {
+            currentSum = num
+            answer += 1
+        }
+    }
+    return answer
+}
+
+
+
+let minimumMaximumSum = function(nums, partitionCount) {
+    let lo = Math.min(...nums), hi = nums.reduce((a,b) => a+b) , answer = -1
+    while (lo <= hi) {
+        let maximumPartitionSumCandidate = Math.floor(lo + (hi - lo)/2)
+        if(minPartitionCount(nums, maximumPartitionSumCandidate) <= partitionCount) {
+            answer = maximumPartitionSumCandidate
+            hi = maximumPartitionSumCandidate - 1
+        } else {
+            lo = maximumPartitionSumCandidate + 1
+        }
+    }
+    return answer
+}
+
+console.log(`For book allocation minimumMaximumSum is ${minimumMaximumSum([12, 34, 67, 90], 2)}`)
+console.log(`For painter partition minimumMaximumSum is ${minimumMaximumSum([10,20,30,40], 2)}`)
+
