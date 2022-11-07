ba9ed9341888f4f5c67cd1a95e19f0fefed68e5e
AnshulYADAV <anshulyadav98@gmail.com>
2022-11-07 23:07:05 +0530

    update on questions

--- 27. Problem Solving on BT and BST(7th Nov 2022)/find-path.js
+++ 27. Problem Solving on BT and BST(7th Nov 2022)/find-path.js
@@ -87,5 +87,62 @@ const getDistance = function(root, p, q) {
     return getUncommonDistance(pathP, pathQ)
 }
 
-console.log(`The distance between ${p} and ${q} is` +
-            ` ${getDistance(root, p, q)}`)
+// console.log(`The distance between ${p} and ${q} is` +
+            // ` ${getDistance(root, p, q)}`)
+
+
+// Add all nodes at distance `distance` from the node to answer
+const subtreeAdd = function(node, distance, answer) {
+    if(node == null) return
+    if(distance == 0) answer.push(node.data)
+    else {
+        subtreeAdd(node.left, distance - 1, answer)
+        subtreeAdd(node.right, distance - 1, answer)
+    }
+}
+            
+
+/**
+ * This function returns the distance of target form the root.
+ * It all also adds all the elements at distance k from the target into
+ * answer.
+ * @param {Node} root 
+ * @param {Number} target 
+ * @param {Array} answer 
+ * @param {Number} k 
+ */
+const dfs = function(root, target, answer, k) {
+    const NOTFOUND = -1
+    if(root == null) return NOTFOUND
+    else if(root.data == target) {
+        subtreeAdd(root, k, answer)
+        return 1
+    }else { // When the root is not the target, check on both children
+        let leftDistance = dfs(root.left, target, answer, k)
+        let rightDistance = dfs(root.right, target, answer, k)
+        if(leftDistance != NOTFOUND) {
+            if(leftDistance == k) answer.push(root.data)
+            let kForRight = k - leftDistance - 1
+            subtreeAdd(root.right, kForRight, answer)
+            return leftDistance + 1
+        }else if(rightDistance != NOTFOUND) {
+            if(rightDistance == k) answer.push(root.data)
+            let kForLeft = k - rightDistance - 1
+            subtreeAdd(root.left, kForLeft, answer)
+            return rightDistance + 1
+        }else{
+            return NOTFOUND
+        }
+    }
+}
+
+const nodesAtDistanceK = function(root, target, k) {
+    let ans = []
+    dfs(root, target, ans, k)
+    return ans
+}
+let k = 3
+x = 2
+
+console.log(`The nodes at distance ${k} from ${x} in tree`, root, 
+            `are ${nodesAtDistanceK(root, x, k)}`)
